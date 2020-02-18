3b2168aa28b3160ba5c7e76cda383d928177dfd4
romaintailhurat <romain.tailhurat@gmail.com>
2020-02-18 11:32:33 +0100

    Implementing a more complex DAG

--- cereal.py
+++ cereal.py
@@ -6,28 +6,56 @@ def load_cereals(context):
     dataset_path = "data/cereal.csv"
     with open(dataset_path) as fd:
         cereals = [row for row in csv.DictReader(fd)]
-    context.log.info(f"There is {len(cereals)} cereals in the dataset {dataset_path}")
     return cereals
 
 @solid
 def sort_by_calories(context, cereals):
     sorted_cereals = list(
-        sorted(cereals, key = lambda cereal: cereal["calories"])
+        sorted(cereals, key = lambda cereal: int(cereal["calories"]))
         )
+    least_caloric = sorted_cereals[0]["name"]
+    most_caloric = sorted_cereals[-1]["name"]
+    return(least_caloric, most_caloric)
+
+@solid
+def sort_by_proteins(context, cereals):
+    sorted_cereals = list(
+        sorted(cereals, key = lambda cereal: int(cereal["protein"]))
+        )
+    least_protein = sorted_cereals[0]["name"]
+    most_protein = sorted_cereals[-1]["name"]
+    return(least_protein, most_protein)
+
+@solid
+def display_results(context, calorie_results, protein_results):
     context.log.info(
         'Least caloric cereal: {least_caloric}'.format(
-            least_caloric=sorted_cereals[0]['name']
+            least_caloric=calorie_results[0]
         )
     )
     context.log.info(
         'Most caloric cereal: {most_caloric}'.format(
-            most_caloric=sorted_cereals[-1]['name']
+            most_caloric=calorie_results[-1]
+        )
+    )
+    context.log.info(
+        'Least protein-rich cereal: {least_protein}'.format(
+            least_protein=protein_results[0]
+        )
+    )
+    context.log.info(
+        'Most protein-rich cereal: {most_protein}'.format(
+            most_protein=protein_results[-1]
         )
     )
 
 @pipeline
 def cereal_pipeline():
-    sort_by_calories(load_cereals())
+    cereals = load_cereals()
+    display_results(
+        calorie_results = sort_by_calories(cereals),
+        protein_results = sort_by_proteins(cereals)
+    )
 
 if __name__ == "__main__":
     result = execute_pipeline(cereal_pipeline)
